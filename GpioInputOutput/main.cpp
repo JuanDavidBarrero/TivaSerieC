07d93c3de95d7db8b709499de074ed27add4dc5e
JuanDavid <Juandbarrero@hotmail.com>
2022-10-26 08:23:53 -0500

    gpio and uart to see status updated

--- GpioInputOutput/main.cpp
+++ GpioInputOutput/main.cpp
@@ -4,8 +4,24 @@
 #include"inc/tm4c123gh6pm.h"
 #include"driverlib/sysctl.h"
 #include"driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/uart.h"
 
 int status;
+
+void initUart(void ){
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART1);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);
+
+    GPIOPinConfigure(GPIO_PC4_U1RX);
+    GPIOPinConfigure(GPIO_PC5_U1TX);
+
+    GPIOPinTypeUART(GPIO_PORTC_BASE, GPIO_PIN_4 | GPIO_PIN_5);
+    UARTConfigSetExpClk(UART1_BASE, SysCtlClockGet(), 115200, (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+
+    UARTEnable(UART1_BASE);
+
+}
 
 
 int main(void)
@@ -22,24 +38,30 @@ int main(void)
     GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1);
 
     GPIOPinTypeGPIOInput(GPIO_PORTE_BASE, GPIO_PIN_1);
-    GPIOPadConfigSet(GPIO_PORTE_BASE, GPIO_PIN_1, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD);
+    GPIOPadConfigSet(GPIO_PORTE_BASE, GPIO_PIN_1, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
 
-
+    initUart();
 
     while (true)
     {
 
         status = GPIOPinRead(GPIO_PORTE_BASE, GPIO_PIN_1);
 
+        UARTCharPutNonBlocking(UART1_BASE,status+48);
+        UARTCharPutNonBlocking(UART1_BASE,'\r');
+        UARTCharPutNonBlocking(UART1_BASE,'\n');
+
+
         if (status == GPIO_PIN_1)
         {
             GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_4,  GPIO_PIN_4);
-            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1,  GPIO_PIN_1);
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 , 0);
                     }
         else
         {
             GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_4 , 0);
-            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 , 0);
+
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1,  2);
 
         }
     }
